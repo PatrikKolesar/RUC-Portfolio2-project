d06b62674410fcb4c836afec19c3fcc09064c1d4
= <pato.kolesar@gmail.com>
2021-11-17 16:53:53 +0100

    titles

---
+++ RUC-Portfolio2-IMDBWebAPI/Application/Services/v1/TitleService/CreateTitleCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using IMDB.Application.DTOs;
+using IMDB.Application.Interfaces.v1.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IMDB.Application.Services.v1.TitleService
+{
+    public class CreateTitleCommandHandler : IRequestHandler<CreateTitleCommand, ResponseMessage>
+    {
+        private readonly ITitlesRepository _titlesRepository;
+
+        public CreateTitleCommandHandler(ITitlesRepository titlesRepository)
+        {
+            _titlesRepository = titlesRepository;
+        }
+
+        public async Task<ResponseMessage> Handle(CreateTitleCommand request, CancellationToken cancellationToken)
+        {
+            return await _titlesRepository.GetTitleByTconst(request.Tconst);
+        }
+    }
+}
--- RUC-Portfolio2-IMDBWebAPI/IMDBWebAPI/Program.cs
+++ RUC-Portfolio2-IMDBWebAPI/IMDBWebAPI/Program.cs
@@ -4,6 +4,7 @@ using IMDB.Application.DTOs;
 using IMDB.Application.Interfaces.v1;
 using IMDB.Application.Interfaces.v1.Repositories;
 using IMDB.Application.Requests;
+using IMDB.Application.Services.v1.TitleService;
 using IMDB.Application.Services.v1.UsersService;
 using IMDB.Application.Services.v1.UsersService.Command;
 using IMDB.Infrastructure.Repositories.v1.AuthService;
@@ -63,10 +64,12 @@ builder.Services.AddDbContext<imdbContext>(ctx => ctx.UseNpgsql(builder.Configur
 //REGISTER REPOSITORIES
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+builder.Services.AddScoped<ITitlesRepository, TitlesRepository>();
 
 //REGISTER HANDLERS
 builder.Services.AddScoped<IRequestHandler<AuthenticateCommand, ResponseMessage>, AuthenticateCommandHandler>();
 builder.Services.AddScoped<IRequestHandler<CreateUserCommand, ResponseMessage>, CreateUserCommandHandler>();
+builder.Services.AddScoped<IRequestHandler<CreateTitleCommand, ResponseMessage>, CreateTitleCommandHandler>();
 
 //JWT
 var tokenKey = builder.Configuration.GetValue<string>("JWTKey");
